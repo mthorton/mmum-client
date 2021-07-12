73535361de655921189bc8ae6dd768cfae58a5d6
Maxwell Horton <maxthomashorton@gmail.com>
2021-07-12 13:12:11 -0400

    add basic fetch functionality to the api js files.

--- src/components/TheSolarSystemAPI.js
+++ src/components/TheSolarSystemAPI.js
@@ -1,7 +1,30 @@
-import { useSate } from 'react';
+import { useState } from 'react';
 
 function TheSolarSystemAPI(){
 
+    let url = 'https://api.le-systeme-solaire.net/en/';
+    const [data, setData] = useState([]);
+
+    const getData = () => {
+        fetch(url)
+        .then(res => res.json())
+        .then(json => {
+            console.log(json);
+            setData(json);
+        })
+    }
+    
+
+    return(
+        <div>
+            <button onClick={getData}>The Solar System API Info</button>
+        </div>
+    )
 }
 
 export default TheSolarSystemAPI;
+
+/*
+RESOURCES:
+website: https://api.le-systeme-solaire.net/en/
+*/
