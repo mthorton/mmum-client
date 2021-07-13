7889832e41c1d35b1008a9d685f7f20a078c19fb
AMJ-Codes <aaronmjeff@gmail.com>
2021-07-13 19:54:51 -0400

    Added landing page.

--- src/App.js
+++ src/App.js
@@ -1,35 +1,24 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.css';
-import './App.css';
-import { makeStyles } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles';
+import { CssBaseline } from '@material-ui/core';
+import Header from './components/Header.js';
 
-import IssLocation from './components/IssLocation';
-import OpenAstronomyAPI from './components/OpenAstronomyAPI';
-import PlanetAPI from './components/PlanetAPI';
-import MoonCalcAPI from './components/MoonCalcAPI';
-import TheSolarSystemAPI from './components/TheSolarSystemAPI';
-import NasaAPI from './components/NasaAPI';
-import GeoLocation from './components/GeoLocation';
 
-function App() {
-  return (
-    <div className="App">
-      test
-      <h1>Master Branch</h1>
-      <h2>Develop Branch</h2>
-      <h3>Max's Branch</h3>
-      <h3>Shavonne's Branch</h3>
-      <GeoLocation/>
-      <IssLocation/>
-      <OpenAstronomyAPI/>
-      <PlanetAPI/>
-      <MoonCalcAPI/>
-      <TheSolarSystemAPI/>
-      <NasaAPI/>
-    </div>
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    minHeight: '100vh',
+    backgroundImage: `url(${process.env.PUBLIC_URL + "assets/starry_sky.jpg"})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover', 
+  },
+}));
+export default function App() {
+  const classes = useStyles();
+  return(
+  <div className={classes.root}>
+    <CssBaseline />
+    <Header />
+  </div>
   );
 }
-
-export default App;
-
-// Aaron's push
