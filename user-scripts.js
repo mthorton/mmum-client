4cb1af84f68285b80c846d89632d373db2380b59
Shavonne Holton <shavonne.holton@gmail.com>
2021-07-12 23:40:43 -0700

    updated user scripts

--- user-scripts.js
+++ user-scripts.js
@@ -1,6 +1,36 @@
 function userSignUp() {
-    console.log('userSignUp Function Called')
-}
+    // console.log('userSignUp Function Called');
+
+    let userName = document.getElementById("userSignup").value;
+    let userPass = document.getElementById("pwdSignup").value;
+
+    let newUserData = {
+        user: {
+            username: userName,
+            password: userPass
+        }
+    };
+
+    console.log(`newUserData --> ${newUserData.user.username} ${newUserData.user.password}`);
+
+    fetch(`http://localhost:3000/user/register`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(newUserData)
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            let token = data.sessionToken;
+            localStorage.setItem('sessionToken', token);
+            tokenChecker();
+        })
+        .catch(err => {
+            console.error(err)
+        })
+};
 
 function userLogin() {
     console.log('userLogin Function Called')
