81ac361cf78cd71577e70890260d73407ca936b1
Shavonne Holton <shavonne.holton@gmail.com>
2021-07-20 18:11:50 -0700

    merged changes to app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 import Footer from './components/site/Footer';
