ac69e70b2832cb9e36a29cd3d5d8266c721064c5
Shavonne Holton <shavonne.holton@gmail.com>
2021-07-20 18:06:43 -0700

    cleaning up tree

--- src/App.js
+++ src/App.js
@@ -3,31 +3,21 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 import Footer from './components/site/Footer';
 import Header from './components/site/Header';
-<<<<<<< HEAD
-import Auth from './components/Auth/Auth';
-import EventIndex from './events/EventIndex';
-=======
-import {
-  BrowserRouter as Router
-} from 'react-router-dom';
+// import {
+//   BrowserRouter as Router
+// } from 'react-router-dom';
 // import Auth from './components/Auth/Auth';
 import AuthMVP from './components/Auth/AuthMVP';
 
-import { useState, useEffect } from 'react';
 import Home from './components/site/Home';
 import NavBar from './home/NavBar';
->>>>>>> ad303cffdf001884e55d3325186d06e8f30289f6
 
 // import { makeStyles } from '@material-ui/styles';
 // import { CssBaseline } from '@material-ui/core';
 
 function App() {
-<<<<<<< HEAD
-  const [sessionToken, setSessionToken] = useState('')
-=======
 
   const [sessionToken, setSessionToken] = useState('');
->>>>>>> ad303cffdf001884e55d3325186d06e8f30289f6
 
   useEffect(() => {
     if (localStorage.getItem('token')){
@@ -41,16 +31,6 @@ function App() {
     console.log(sessionToken);
   }
 
-<<<<<<< HEAD
-  // const clearToken = () => {
-  //   localStorage.clear();
-  //   setSessionToken('');
-  // }
-
-  const protectedViews = () => {
-    return (sessionToken === localStorage.getItem('token') ? <EventIndex token={sessionToken}/>
-    : <Auth updateToken={updateToken}/>)
-=======
   const clearToken = () => {
     localStorage.clear();
     setSessionToken('');
@@ -59,19 +39,12 @@ function App() {
   const protectedViews = () => {
     return (sessionToken === localStorage.getItem('token') ? <Home token={sessionToken}/>
     : <AuthMVP updateToken={updateToken}/>)
->>>>>>> ad303cffdf001884e55d3325186d06e8f30289f6
   }
 
   return(
     <div className='App'>
-<<<<<<< HEAD
-      <Header />
-      {protectedViews()}
-      <Footer />
-=======
       <NavBar clickLogout={clearToken}/>
       {protectedViews()}
->>>>>>> ad303cffdf001884e55d3325186d06e8f30289f6
     </div>
   );
 }
