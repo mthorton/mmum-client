9b571f20d36922c5e877b64003b291d5de50f435
Shavonne Holton <shavonne.holton@gmail.com>
2021-07-22 19:05:55 -0700

    updated APIURL

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -5,7 +5,7 @@ switch (window.location.hostname) {
     case 'localhost' || '127.0.0.1':
         break;
     case 'meet-mum-client.herokuapp.com/':
-        APIURL = 'https://meet-mum-client.herokuapp.com/'
+        APIURL = 'https://meet-mum.herokuapp.com'
 }
 
 export default APIURL;
